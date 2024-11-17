697cccbd8732479232757cdbba85fe803fd352d6
Giffulf <giffulf@mail.ru>
2024-11-17 21:58:58 +0300

    commit_1

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, HTTPException
+import requests
+from config import API_URL
+
+app = FastAPI()
+
+@app.get("/weather/{city}")
+def get_weather(city: str):
+    try:
+        response = requests.get(f"{API_URL}/{city}")
+        response.raise_for_status()  # Проверка на ошибки HTTP
+
+        result = response.json()
+        return result
+
+    except requests.exceptions.HTTPError as http_err:
+        raise HTTPException(status_code=response.status_code, detail=str(http_err))
+    except Exception as err:
+        raise HTTPException(status_code=500, detail=str(err))
